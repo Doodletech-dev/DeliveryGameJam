f11637fde926cf632d915521608a79696bcb74f0
fabioduck <adam@doodletech.dev>
2024-12-27 03:17:08 -0600

    fix jitter bug
    _process was needed for bullet movement instead of _physics_process when using a particle emitter

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 800.0
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	velocity = speed * get_global_transform().basis_xform(Vector2.RIGHT)
 	move_and_slide()
 
