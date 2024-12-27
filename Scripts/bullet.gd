0ddf9a1f60f9d00c31f556dee6c6ff6ab57848b6
fabioduck <adam@doodletech.dev>
2024-12-27 02:58:30 -0600

    Remove bullets when off screen

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -5,3 +5,7 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	velocity = speed * get_global_transform().basis_xform(Vector2.RIGHT)
 	move_and_slide()
+
+
+func _on_visible_on_screen_notifier_screen_exited() -> void:
+	queue_free()
