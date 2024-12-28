cc74271c2f7a1d58f55c4b74233b811fbf399458
fabioduck <adam@doodletech.dev>
2024-12-28 17:02:53 -0600

    Revert "Merge branch 'more-turrets'"
    This reverts commit 0b168b0b645f08b476149c2dae086cbf13b6927e, reversing
    changes made to 162ef34218dd5020a764054a8906fa3377c3edad.

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,17 +1,10 @@
 extends CharacterBody2D
 class_name Bullet
 
-@export var speed = 1500.0
+@export var speed = 800.0
 @export var damage = 1
-@export var trail_effect: PackedScene
-
-var effect_instance
-func _ready():
-	effect_instance = trail_effect.instantiate()
-	get_tree().get_root().add_child(effect_instance)
 
 func _process(delta: float) -> void:
-	effect_instance.global_transform = global_transform
 	velocity = speed * get_global_transform().basis_xform(Vector2.RIGHT)
 	move_and_slide()
 
