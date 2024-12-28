273cdff3349ce64e48f6aa01b5e59820c43b4665
IceSparkStudios <icesparkstudios@gmail.com>
2024-12-27 20:32:49 -0500

    Added Moving Toward Nuke Target and Moving Toward a Random Point on a Random Path

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -3,16 +3,23 @@ extends Node2D
 @export var enemy_to_spawn : Array[PackedScene] = []
 @export var spawn_weight : Array[float] = []
 
+@export var target : Node2D
+
 @export var min_spawn_time = 3
 @export var max_spawn_time = 6
+
+@export var ai_paths : Array[Path2D] = []
 
 @onready var spawn_timer = $SpawnTimer
 
 var spawn_weightf
+var target_position
 
 func _ready():
 	if(enemy_to_spawn.size() != spawn_weight.size()):
 		printerr("Enemy to spawn and spawn weights are not equal. Please check that each spawned enemy has a weight assigned")
+	
+	target_position = target.global_position
 	_begin_spawn_timer()
 	
 func _begin_spawn_timer():
@@ -29,6 +36,8 @@ func _on_spawn_timer_timeout():
 	for i in enemy_to_spawn.size():
 		if(rang < spawn_weightf):
 			var enemy_spawned = enemy_to_spawn[i].instantiate()
+			enemy_spawned._set_target_position(target_position)
+			enemy_spawned._get_ai_paths(ai_paths)
 			enemy_spawned.global_position = global_position
 			add_child(enemy_spawned)
 			_begin_spawn_timer()
