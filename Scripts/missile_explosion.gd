aaae527aa79b6d90137a35e023c910ebb18ce4c2
fabioduck <adam@doodletech.dev>
2024-12-31 15:09:15 -0600

    fixed missile and simple laser

--- Scripts/missile_explosion.gd
+++ Scripts/missile_explosion.gd
@@ -1,4 +1,10 @@
 extends Node2D
+@onready var fire: GPUParticles2D = $Explosion_Fire
+@onready var smoke: GPUParticles2D = $Explosion_Smoke
+
+func trigger():
+	fire.emitting = true
+	smoke.emitting = true
 
 func _on_explosion_smoke_finished() -> void:
 	queue_free()
