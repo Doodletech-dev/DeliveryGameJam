71fe8f477fcfb712114573384bc98e8f4eeb620b
IceSparkStudios <67023569+IceSparkStudios@users.noreply.github.com>
2025-01-02 20:41:28 -0500

    Fixed deleting save?

--- Scenes/UI/GameStart/main_menu.gd
+++ Scenes/UI/GameStart/main_menu.gd
@@ -12,8 +12,10 @@ func _ready() -> void:
 
 func _on_button_new_game_pressed() -> void:
 	GameManager.saver_loder._delete_save_game()
-	get_tree().change_scene_to_packed(intro_scene)
+	call_deferred("_scene_change")
 
+func _scene_change():
+	get_tree().change_scene_to_packed(intro_scene)
 
 func _on_button_exit_pressed() -> void:
 	get_tree().quit()
