ed3f407454d4045a15db406907cac77bc2cc3a93
fabioduck <adam@doodletech.dev>
2025-01-02 19:32:36 -0600

    laser turret upgrade adjusted

--- Scripts/cargo.gd
+++ Scripts/cargo.gd
@@ -8,19 +8,25 @@ class_name Cargo
 @export var death_effect: PackedScene
 @export var max_repair = 50
 @export var repair_speed = 0.5
+@export var health_upgrade_amount = 0.25
 var repaired_amount = 0
 var never_shielded = true
 var never_repaired = true 
 var player_current_health
 var player_max_health = 100
+var health_modifier = 1
 
 func _ready():
 	if(GameManager.saver_loder._load_game()):
 		pass
 	else:
-		player_current_health = player_max_health
+		var health_multiplier = health_modifier
+		if(GameManager.walker_upgrades > 0):
+			health_multiplier *= 1 + health_upgrade_amount * GameManager.walker_upgrades
+		player_current_health = hitbox.max_health * health_modifier
 	print ("Player Current Health is " + str(player_current_health))
 	GameManager.current_health = hitbox.max_health
+	GameManager.max_health = hitbox.max_health
 
 func _on_save_game():
 	var data = {}
@@ -30,6 +36,12 @@ func _on_save_game():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	if(GameManager.health_upgrade_pending):
+		GameManager.health_upgrade_pending = false
+		print( 1 + health_upgrade_amount * GameManager.walker_upgrades)
+		player_current_health *= 1 + health_upgrade_amount
+		hitbox.current_health *= 1 + health_upgrade_amount
+		GameManager.max_health = hitbox.max_health * (1 + health_upgrade_amount)
 	GameManager.current_health = hitbox.current_health
 	if(GameManager.shield_purchased and never_shielded):
 		never_shielded = false
@@ -47,6 +59,7 @@ func _physics_process(delta: float) -> void:
 		if(hitbox.current_health > hitbox.max_health):
 			hitbox.current_health = hitbox.max_health
 			GameManager.repairing = false
+	
 
 func _on_load_game(data : Dictionary):
 	player_current_health =	 data["player_health"]
